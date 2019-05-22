aa24a5305b33cedc00291e5ba0a1f1b2196c8fc0
keita69sawada <keita69sawada@gmail.com>
2019-05-22 23:47:57 +0900

    "",''のsurroundキーを実装

--- keyboards/ergodash/rev2/keymaps/default/keymap.c
+++ keyboards/ergodash/rev2/keymaps/default/keymap.c
@@ -23,6 +23,8 @@ enum custom_keycodes {
   SD_CBR,  // Surround {}
   SD_CRB,  // Surround []
   SD_PRN,  // Surround ()
+  SD_SCLN, // Surround ""
+  SD_QUOT, // Surround ''
 };
 
 #define EISU   LALT(KC_GRV)
@@ -62,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,----------------------------------------------------------------------------------------------------------------------.
      * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |                    |   }  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
      * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-     * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |  {}  |  []  |  ()  |   )  |  |   |
+     * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |  {}  |  []  |  ()  |  ""  |  ''  |
      * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
      * | Ctrl |      |      |      | EISU |      |      |                    |      | Left | Down |  Up  | Right| Home | End  |
      * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
@@ -73,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_LOWER] = LAYOUT(
       KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, SD_CBR , SD_CRB , SD_PRN , KC_RPRN, KC_PIPE, \
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, SD_CBR , SD_CRB , SD_PRN , SD_SCLN, SD_QUOT, \
       KC_LCTL, _______, _______, _______, EISU,    _______, _______,                        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END,  \
       KC_LSFT, _______, _______, _______, _______, _______,          KC_SPC,       KC_ENT ,          _______, _______, _______, _______, _______, _______, \
       _______, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC , KC_DEL,       KC_BSPC, KC_ENT , RAISE,            EISU,    GUI_RT,  GUI_UP,  _______  \
@@ -195,6 +197,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // when keycode this is pressed to surround ()
         SEND_STRING("(");
         SEND_STRING(")");
+        SEND_STRING(SS_TAP(X_RIGHT));
+      } else {
+        // when keycode this is pressed
+      }
+      return false;
+      break;
+    case SD_SCLN:
+      if (record->event.pressed) {
+        // when keycode this is pressed to surround ""
+        SEND_STRING("\"");
+        SEND_STRING("\"");
+        SEND_STRING(SS_TAP(X_RIGHT));
+      } else {
+        // when keycode this is pressed
+      }
+      return false;
+      break;
+    case SD_QUOT:
+      if (record->event.pressed) {
+        // when keycode this is pressed to surround ''
+        SEND_STRING("'");
+        SEND_STRING("'");
         SEND_STRING(SS_TAP(X_RIGHT));
       } else {
         // when keycode this is pressed
