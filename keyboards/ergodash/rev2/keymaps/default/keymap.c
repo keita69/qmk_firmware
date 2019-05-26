7fd41fa90f12bc14abce4a7a9c8b88cec4efeab9
keita69sawada <keita69sawada@gmail.com>
2019-05-26 21:34:52 +0900

    Windows keyを左にし、ALT＋TABを右側に寄せた

--- keyboards/ergodash/rev2/keymaps/default/keymap.c
+++ keyboards/ergodash/rev2/keymaps/default/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |      |ClAlDl|      |K8SCTL|      |   N  |   M  |   ,  |   .  |   /  |      |
      * |-------------+------+------+------+------| Space|------+------+------+ Enter|------+------+------+------+-------------|
-     * |      |  GUI |  ALt |ALT_TB|||||||| Lower|      |  Del |||||||| Bksp |      | Raise||||||||  GUI |GUI_RT|GUI_UP|K8SCTL|
+     * |      |  GUI |  ALt |  GUI |||||||| Lower|      |  Del |||||||| Bksp |      | Raise||||||||ALT_TB|GUI_RT|GUI_UP|K8SCTL|
      * ,----------------------------------------------------------------------------------------------------------------------.
      */
     [_QWERTY] = LAYOUT( \
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        ALT2F,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             CLALDL,       K8SCTL ,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, \
-      _______, KC_LGUI, KC_LALT, ALT_TB,           LOWER,   KC_SPC , KC_DEL,       KC_BSPC, KC_ENT , RAISE,            KC_RGUI, GUI_RT,  GUI_UP,  K8SCTL   \
+      _______, KC_LGUI, KC_LALT, KC_LGUI,          LOWER,   KC_SPC , KC_DEL,       KC_BSPC, KC_ENT , RAISE,            ALT_TB,  GUI_RT,  GUI_UP,  K8SCTL   \
     ),
 
     /* Lower
