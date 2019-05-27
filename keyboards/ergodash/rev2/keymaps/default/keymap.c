d5b2a029abdb8d2b7f0e7f6674c95ec3ba5b3bac
keita69sawada <keita69sawada@gmail.com>
2019-05-27 22:45:17 +0900

    mouseキー操作とmouseホイール操作の位置を入れ替えた

--- keyboards/ergodash/rev2/keymaps/default/keymap.c
+++ keyboards/ergodash/rev2/keymaps/default/keymap.c
@@ -86,20 +86,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,----------------------------------------------------------------------------------------------------------------------.
      * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |                    |   }  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
      * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-     * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |      |WhLeft|WhDown| WhUp |WhRght|      |      |
+     * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |      |MsLeft|MsDown| MsUp |MsRght|MsBtn1|MsBtn2|
      * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
      * | Ctrl |      |      |      |      |      |      |                    |CTL_W | Left | Down |  Up  | Right| Home | End  |
      * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-     * | Shift|      |      |      |      |      |      | Space|      | Enter|      |MsLeft|MsDown| MsUp |MsRght|MsBtn1|MsBtn2|
+     * | Shift|      |      |      |      |      |      | Space|      | Enter|      |WhLeft|WhDown| WhUp |WhRght|      |      |
      * |-------------+------+------+------+------| Space|------+------+------+ Enter|------+------+------+------+-------------|
      * |      |  GUI |  ALt | EISU |||||||| Lower|      |  Del |||||||| Bksp |      | Raise|||||||| EISU |GUI_RT|GUI_UP|      |
      * ,----------------------------------------------------------------------------------------------------------------------.
      */
     [_RAISE] = LAYOUT(
       KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, \
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1, KC_BTN2, \
       KC_LCTL, _______, _______, _______, _______, _______, KC_DEL ,                        CTL_W,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END,  \
-      KC_LSFT, _______, _______, _______, _______, _______,          KC_SPC,       KC_ENT ,          KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1, KC_BTN2, \
+      KC_LSFT, _______, _______, _______, _______, _______,          KC_SPC,       KC_ENT ,          KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, \
       _______, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC , KC_DEL,       KC_BSPC, KC_ENT , RAISE,            EISU,    GUI_RT,  GUI_UP,  _______  \
     ),
 
